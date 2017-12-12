bd8a107614541bd0eba4d3d8e5fc0847ab2e054c
Bruno <brunodemore@hotmail.com>
2017-12-12 13:07:59 -0500

    Site Master updated

--- Comp229-Assign04/Site.Master.designer.cs
+++ Comp229-Assign04/Site.Master.designer.cs
@@ -7,22 +7,20 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Comp229_Assign04
-{
-
-
-    public partial class SiteMaster
-    {
-
+namespace Comp229_Assign04 {
+    
+    
+    public partial class SiteMaster {
+        
         /// <summary>
-        /// HeadContent control.
+        /// Image1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
-
+        protected global::System.Web.UI.WebControls.Image Image1;
+        
         /// <summary>
         /// MainContent control.
         /// </summary>
